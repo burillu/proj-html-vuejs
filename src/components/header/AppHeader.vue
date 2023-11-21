b7763af32fe04be1ac60faca71e5c76208c6403b
burillu <burillu90@hotmail.it>
2023-11-21 21:55:23 +0100

    create nav-bar static

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,6 +1,57 @@
 <template>
-    <div>
-        <h2>Header</h2>
+    <div class="container my-bg-primary d-flex justify-content-between align-items-center">
+        <div class="my-logo">
+            <img src="../../assets/images/logo.png" alt="logo">
+        </div>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+
+
+                <div class=" " id="navbarNavDropdown">
+                    <ul class="navbar-nav fw-bold ">
+                        <li class="nav-item  ">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                Our Process
+                            </a>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                Pricing
+                            </a>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                Blog
+                            </a>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                Contact
+                            </a>
+
+                        </li>
+                        <li class="nav-item">
+                            <button class="my-btn my-btn-light">
+                                Get a Quote
+                            </button>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
 </template>
 
@@ -10,4 +61,10 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../../assets/styles/partials/variables' as *;
+
+li>a {
+    color: $color-my-secondary;
+}
+</style>
