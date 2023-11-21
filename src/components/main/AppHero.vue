a4a0ef74bcd0a7a88a34523025bb2d9a1b0421ed
burillu <burillu90@hotmail.it>
2023-11-21 18:32:49 +0100

    structure base hero

--- src/components/main/AppHero.vue
+++ src/components/main/AppHero.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
-        <h2>Hero</h2>
-    </div>
+    <section class="my-bg-hero my-text-secondary">
+        <div class="container text-center">
+            <span class="my-text-contrast fw-bold">
+                Imagine, Create, Esperience
+            </span>
+            <h1 class="fw-bold">We Animate <br>
+                With Ninja-Like Precision</h1>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, incidunt?</span><br>
+            <button class="my-btn my-btn-light">Start any</button>
+            <button class="my-btn my-btn-primary"> any</button>
+        </div>
+    </section>
 </template>
 
 <script>
+
 export default {
     name: 'AppHero'
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+    height: 100vh;
+}
+</style>
