1f2747629fe999e08dabb9e1168649d7e7d9647b
burillu <burillu90@hotmail.it>
2023-11-21 17:01:09 +0100

    scaffolding with vite, bootstrap, sass, fontawesom

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-center">Hello world <i class="fa-solid fa-wifi fa-shake"></i></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped></style>
