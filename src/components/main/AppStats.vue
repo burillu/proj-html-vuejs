0418defe4cfb5f4cf089afd34b65991fe6a05b6f
burillu <burillu90@hotmail.it>
2023-11-21 23:11:29 +0100

    structured counter section

--- src/components/main/AppStats.vue
+++ src/components/main/AppStats.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <h2>Stats</h2>
-    </div>
+    <section class="my-bg-counter my-text-secondary text-center fw-bold">
+        <div class="container">
+            <h2 class="fw-bold">
+                Anidio QUick Facts
+            </h2>
+            <div class="row justify-content-between ">
+                <div class="col-2 rounded-3 border d-flex flex-column justify-content-center align-items-center py-2"
+                    v-for="item in 4">
+                    <div><span class="num-counter">100+</span></div>
+                    <div><span class="my-text-contrast">bla bla bla</span> </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <script>
@@ -10,4 +22,12 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+    height: 40vh;
+}
+
+.num-counter {
+    font-size: 2.8em;
+}
+</style>
