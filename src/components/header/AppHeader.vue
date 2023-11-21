f1f8b57f3cf9b7234bdfeea37fdf9ff333200e49
burillu <burillu90@hotmail.it>
2023-11-21 21:57:35 +0100

    add inner container

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,57 +1,60 @@
 <template>
-    <div class="container my-bg-primary d-flex justify-content-between align-items-center">
-        <div class="my-logo">
-            <img src="../../assets/images/logo.png" alt="logo">
-        </div>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-
-
-                <div class=" " id="navbarNavDropdown">
-                    <ul class="navbar-nav fw-bold ">
-                        <li class="nav-item  ">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                Our Process
-                            </a>
-
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                Pricing
-                            </a>
-
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                Blog
-                            </a>
-
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                Contact
-                            </a>
-
-                        </li>
-                        <li class="nav-item">
-                            <button class="my-btn my-btn-light">
-                                Get a Quote
-                            </button>
-                        </li>
-
-                    </ul>
-                </div>
+    <div class="my-bg-primary ">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="my-logo">
+                <img src="../../assets/images/logo.png" alt="logo">
             </div>
-        </nav>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+
+
+                    <div class=" " id="navbarNavDropdown">
+                        <ul class="navbar-nav fw-bold ">
+                            <li class="nav-item  ">
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                    Our Process
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                    Pricing
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                    Blog
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" role="button" aria-expanded="false">
+                                    Contact
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <button class="my-btn my-btn-light">
+                                    Get a Quote
+                                </button>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
     </div>
 </template>
 
