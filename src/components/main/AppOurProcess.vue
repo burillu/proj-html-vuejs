435f2dc36b941afdbc10e17ce0c44a35890fa3ed
burillu <burillu90@hotmail.it>
2023-11-22 10:16:03 +0100

    add content and card

--- src/components/main/AppOurProcess.vue
+++ src/components/main/AppOurProcess.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-        <h2>Our Process</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <img :src="'./images/process-thumb.png'" alt="process-thumb">
+            </div>
+            <div class="col-6">
+                <AppTitleSection />
+                <div class="row">
+                    <div v-for="item in 2" class="col-6">
+                        AppCard carusel
+                        <AppCard />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+import AppCard from './AppCard.vue';
+import AppTitleSection from './AppTitleSection.vue';
+
 export default {
-    name: 'AppOurProcess'
+    name: "AppOurProcess",
+    components: { AppTitleSection, AppCard }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div.container {
+    position: relative;
+
+    img {
+        position: absolute;
+        left: -35%;
+    }
+}
+</style>
