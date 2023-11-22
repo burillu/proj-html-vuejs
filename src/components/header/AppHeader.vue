52d861b9df7fd8bc47f3796c98da88de4e6836c6
burillu <burillu90@hotmail.it>
2023-11-22 12:43:06 +0100

    add navbar component in header

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,73 +1,23 @@
 <template>
     <div class="my-bg-primary ">
-        <div class="container d-flex justify-content-between align-items-center">
-            <div class="my-logo">
-                <img src="../../assets/images/logo.png" alt="logo">
-            </div>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-
-
-                    <div class=" " id="navbarNavDropdown">
-                        <ul class="navbar-nav fw-bold ">
-                            <li class="nav-item  ">
-                                <a class="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Portfolio</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                    Our Process
-                                </a>
-
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                    Pricing
-                                </a>
-
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                    Blog
-                                </a>
-
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" role="button" aria-expanded="false">
-                                    Contact
-                                </a>
-
-                            </li>
-                            <li class="nav-item">
-                                <button class="my-btn my-btn-light">
-                                    Get a Quote
-                                </button>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <AppNavBar :classTextColor="classTextColor" :src="srcLogo" />
 
     </div>
 </template>
 
 <script>
+import AppNavBar from './AppNavBar.vue';
+
 export default {
-    name: 'AppHeader'
+    name: "AppHeader",
+    components: { AppNavBar },
+    data() {
+        return {
+            classTextColor: 'my-text-secondary',
+            srcLogo: './images/logo.png'
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped>
-@use '../../assets/styles/partials/variables' as *;
-
-li>a {
-    color: $color-my-secondary;
-}
-</style>
+<style lang="scss" scoped></style>
