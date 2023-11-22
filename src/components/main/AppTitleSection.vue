8db98ee777e9373bfb37c65dae5d8f28ef42101c
burillu <burillu90@hotmail.it>
2023-11-22 16:02:44 +0100

    created dynamic titles with store

--- src/components/main/AppTitleSection.vue
+++ src/components/main/AppTitleSection.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class="title-section mb-3">
-        <span class="my-text-terciary fw-bold">
-            Imagine, Create, Esperience
+    <div class="mb-3" :class="{ 'title-section': sectionProps.center }">
+        <span class="fw-bold" :class="sectionProps.classHead">
+            {{ sectionProps.headText }}
         </span>
-        <h2 class="fw-bold">We Animate <br>
-            With Ninja-Like Precision</h2>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, incidunt?</span>
+        <h2 class="fw-bold" :class="sectionProps.classText">{{ sectionProps.textH2 }}</h2>
+        <span :class="sectionProps.classText">{{ sectionProps.subtitle }}</span>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'AppTitleSection'
+    name: 'AppTitleSection',
+    props: {
+        sectionProps: Object,
+    },
+    created() {
+        console.log(this.sectionProps);
+    }
 }
 </script>
 
--- src/data/store.js
+++ src/data/store.js
@@ -70,5 +70,58 @@ export const store = reactive({
         ],
 
     },
+    titleSection: {
+        present: {
+            classHead: 'my-text-terciary',
+            classText: 'my-text-primary',
+            center: true,
+            headText: 'Our Love For Animation',
+            textH2: 'We\'re Storytelling Expert With Creative Smarts',
+            subtitle: 'Our culture inspires the creative and innovative spirit found throughout our Studio. We breathe life into new ideas!'
+        },
+        feat: {
+            classHead: 'my-text-terciary',
+            classText: 'my-text-primary',
+            center: true,
+            headText: 'Our Works',
+            textH2: 'Featured Productions',
+            subtitle: 'Here\'s just a small sample of some of those projects that we\'re quite proud of. If you\'re looking for something specific feel free to get in contact with us.'
+        },
+        ourProcess: {
+            classHead: 'my-text-terciary',
+            classText: 'my-text-primary',
+            center: false,
+            headText: 'Our Process',
+            textH2: 'Our Process for Your Animation Production',
+            subtitle: 'We have an effective process for working on animation'
+
+        },
+        ourTeam: {
+            classHead: 'my-text-terciary',
+            classText: 'my-text-primary',
+            center: true,
+            headText: 'Anidio Magic Team Member',
+            textH2: 'Let Us Animate Your Project',
+            subtitle: 'We create new worlds!Let’s collaborate and create engaging, effective, award-winning animations'
+        },
+        whyUs: {
+            classHead: 'my-text-terciary',
+            classText: 'my-text-primary',
+            center: true,
+            headText: 'Why Anidio studio?',
+            textH2: 'We Love Our Clients',
+            subtitle: 'We are trusted throughout adland and have a wealth of recommendations from start-ups to renowned global brands.'
+        },
+        contactUs: {
+            classHead: 'my-text-contrast',
+            classText: 'my-text-secondary',
+            center: true,
+            headText: 'We Create New Worlds!',
+            textH2: 'Want Tips On Your Storytelling?',
+            subtitle: 'Join our newsletter for tips on how to elevate your brand through storytelling'
+        },
+
+
+    }
 
 })
