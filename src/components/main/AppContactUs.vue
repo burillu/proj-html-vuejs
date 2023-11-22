3e526834d2d516f9829af98bcdd5842751e4395d
burillu <burillu90@hotmail.it>
2023-11-22 10:52:21 +0100

    add content in contact us

--- src/components/main/AppContactUs.vue
+++ src/components/main/AppContactUs.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>
-        <h2>Contact Us</h2>
-    </div>
+    <section class="my-bg-counter">
+        <div class="container py-5">
+            <AppTitleSection />
+
+            <div class="mb-3">
+
+                <input type="text" class="form-control rounded-5" id="exampleFormControlInput1" placeholder="submit">
+            </div>
+
+        </div>
+    </section>
 </template>
 
 <script>
+import AppTitleSection from './AppTitleSection.vue';
+
 export default {
-    name: 'AppContactUs'
+    name: "AppContactUs",
+    data() {
+        return {
+            classTitle: "my-text-terciary"
+        };
+    },
+    components: { AppTitleSection }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+    width: 80%;
+    margin: 0 auto;
+    //text-align: center;
+}
+</style>
