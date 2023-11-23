0492ac22b9c3a60de013b27ce6a957079d0d08e3
burillu <burillu90@hotmail.it>
2023-11-23 12:59:14 +0100

    add content in appcardstats

---
+++ src/components/main/AppCardStats.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="border rounded-3 d-flex flex-column justify-content-center align-items-center py-2">
+        <div><span class="num-counter fw-bold">{{ cardProps.data }}</span></div>
+        <div><span class="my-text-contrast">{{ cardProps.text }}</span> </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppCardStats',
+    props: {
+        cardProps: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.num-counter {
+    font-size: 2.8em;
+}
+</style>
