f968f6e80e512d5ae1cd1975324e314cdb910933
burillu <burillu90@hotmail.it>
2023-11-23 14:53:57 +0100

    add bg class in main, add loader component

---
+++ src/components/main/AppLoaderIntro.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="my-bg-loader">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppLoaderIntro',
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    height: 100vh;
+}
+</style>
