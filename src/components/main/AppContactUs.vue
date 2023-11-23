5084b9ce90c5b38c1c4f2f90102423187d32dddf
burillu <burillu90@hotmail.it>
2023-11-23 13:22:37 +0100

    improve section contactUs

--- src/components/main/AppContactUs.vue
+++ src/components/main/AppContactUs.vue
@@ -1,14 +1,21 @@
 <template>
     <section class="my-bg-counter">
+        <img :src="'./images/shape-top.png'" alt="shape-top" class="shape">
         <div class="container py-5">
             <AppTitleSection :sectionProps="store.titleSection.contactUs" />
 
-            <div class="mb-3">
+            <div class="mb-3 input-container">
 
-                <input type="text" class="form-control rounded-5" id="exampleFormControlInput1" placeholder="submit">
+                <div class="input-group mb-3">
+
+                    <input type="email" class="form-control rounded-start-5" placeholder="Insert your email"
+                        aria-label="Username" aria-describedby="basic-addon1">
+                    <span class="input-group-text rounded-end-5" id="basic-addon1">Submit</span>
+                </div>
             </div>
 
         </div>
+        <img :src="'./images/shape-bottom.png'" alt="shape-bottom" class="shape-bottom">
     </section>
 </template>
 
@@ -29,9 +36,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-input {
+section {
+    position: relative;
+}
+
+.input-container {
     width: 80%;
     margin: 0 auto;
-    //text-align: center;
+
+    span {
+        cursor: pointer;
+    }
+
+
+
+}
+
+.shape-bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+}
+
+.shape {
+    position: relative;
+    z-index: 100;
 }
 </style>
